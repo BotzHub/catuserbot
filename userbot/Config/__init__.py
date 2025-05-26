fa7549f9d15eeba732a7395a260892aac3cee96e
Botz Hub <botzhub01@gmail.com>
2025-05-26 23:47:03 +0530

    Update __init__.py

--- userbot/Config/__init__.py
+++ userbot/Config/__init__.py
@@ -7,4 +7,5 @@
 # Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-from .catub_config import Config
+from .catub_config import Config as Config
+
